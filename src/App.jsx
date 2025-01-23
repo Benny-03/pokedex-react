73b178b8a31b19adf7a02b1b30d62f094861eada
Ferdinando <contiferdinando02@gmail.com>
2025-01-23 11:36:59 +0100

    inizio

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,43 @@ const App = () => {
     const [count, setCount] = useState(0);
 
     return (
-        <div>
-            {count} <button onClick={() => {
-                setCount(count + 1)
-            }
-            }>
-                +
-            </button>
+        // <div>
+        //     {count} <button onClick={() => {
+        //         setCount(count + 1)
+        //     }
+        //     }>
+        //         +
+        //     </button>
+        // </div>
+        <div className='home-page'>
+            <div className='griglia-tot'>
+                <div className='scheda'>
+                    <div className='immagine'>
+                        <img src='' alt='pokemon'/>
+                    </div>
+                    <div className='info'>
+                        <p>Numero: 001</p>
+                        <p>Bulbasaur</p>
+                        <div className='abilita'>
+
+                        </div>
+                    </div>
+                </div>
+                <div className='scheda'>
+                    <div className='immagine'>
+                        <img src='' alt='pokemon'/>
+                    </div>
+                    <div className='info'>
+                        <p>Numero: 001</p>
+                        <p>Bulbasaur</p>
+                        <div className='abilita'>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+        
     );
 }
 
